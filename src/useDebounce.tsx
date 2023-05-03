351bb772b85e1a04386b9cecc489c471d9fa6141
Vito Paparella Santorsola <vs.paparella@cybsec.it>
2023-05-03 18:24:37 +0200

    Correzione 7: corretto il tipo nello useDebounce

--- src/useDebounce.tsx
+++ src/useDebounce.tsx
@@ -1,23 +1,24 @@
-import {useEffect, useState} from "react";
-import {clearTimeout} from "timers";
+import { useEffect, useState } from "react";
+import { clearTimeout } from "timers";
 
-interface DebounceItem{
-    value: number;
-    delay: number;
+interface DebounceItem<T> {
+  value: T;
+  delay: number;
 }
+
 /*attraverso questo useDebounce è possibile settare un tempo massimo di fetch di API, dopo il quale interrompere la ricerca*/
-function useDebounce ({value, delay}:DebounceItem):number {
-    const [debouncedValue, setDebouncedValue] = useState(value);
+function useDebounce<T>({ value, delay }: DebounceItem<T>): T {
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
-    useEffect(() => {
-        const handler = setTimeout(() =>{
-            setDebouncedValue(value);
-        }, delay);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
 
-        return (() => clearTimeout(handler))
-    }, [value, delay]);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
 
-    return debouncedValue;
+  return debouncedValue;
 }
 
 export default useDebounce;
